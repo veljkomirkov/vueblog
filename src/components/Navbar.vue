c6adf200a271e9deb68ba86e467e833c9a2c0c40
Veljko Mirkov <veljkomirkov@gmail.com>
2020-10-09 20:25:37 +0200

    PrviZadatak

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    
+    <div class="navbar-nav">
+     <router-link class="nav-item nav-link" to="/posts">Posts</router-link>
+      
+    </div>
+  </nav>
+
+  
+  
+</template>
+
+
+<script>
+export default {
+  name: 'Navbar',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import AppPosts from './components/AppPosts.vue'
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/posts' },
+  { path: '/posts', component: AppPosts, name: 'Posts' },
+  
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
